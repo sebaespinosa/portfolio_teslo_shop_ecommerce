0b8ee588363f6219bcf3c5fdbee1e575f516b2d8
seba espinosa <sebaespinosa@gmail.com>
2024-10-18 10:53:25 -0300

    fix de warnings pre build

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { inter } from "@/config/fonts";
+
+import "./globals.css";
+import { Providers } from "@/components";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Demo Teslo | Shop",
+    default: "Home - Teslo | Shop",
+  },
+  description: "Demo tienda Ecommerce con NextJS 14 y TailwindCSS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
